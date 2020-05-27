f684df67398efefd29e95eda17cbd4e4ecc2aac5
baymux <byy5912@163.com>
2020-05-27 21:00:57 +0800

    add：CorsConfig.java 添加全局跨域配置

---
+++ mall-gateway/src/main/java/com/bay/mall/gateway/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.bay.mall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+/**
+ * @ClassName CorsConfiguration
+ * @Description TODO 配置全局跨域
+ * @Author baymux
+ * @Date 2020/5/24 12:39
+ * @Vsrsion 1.0
+ **/
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        // 配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return new CorsWebFilter(source);
+    }
+}
